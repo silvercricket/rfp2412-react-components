73de2c666f693fe6b741e8b020f922196dd13fc2
AustinMallison <silvercricket101@hotmail.com>
2024-12-20 12:37:26 -0800

    opened wrong directory at first, lol

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // TODO
+//var food = ['rice','beans'];
+//var rice = 'rice';
+// var beans = 'beans';
+// const Item1 = () => (
+//   <li>rice</li>
+// );
+
+// const Item2 = () => (
+//   <li>beans</li>
+// );
+const { useState } = React;
+
+const Grocery = (props) => {
+  const [isBold, setIsBold] = useState(false);
+
+  const style = {
+    fontWeight: isBold ? 'bold' : 'normal',
+  };
+  return (
+    <li style = {style} onMouseEnter = {() => setIsBold(true)}>
+      {props.food}
+    </li>
+  );
+};
+const Groceries = (props) => (
+  <ul>
+    {props.groceryItems.map((food) =>
+      <Grocery food={food} />
+    )}
+  </ul>
+);
+
+const GroceryList = () => (
+  <div>
+  <h2>GroceryList </h2>
+    <Groceries groceryItems={['rice','beans','tomato']} />
+  </div>
+);
+ReactDOM.render(
+  <GroceryList />,
+  document.getElementById(
+    'app'
+  )
+);
